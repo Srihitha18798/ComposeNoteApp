2b13a5576fc751c2fcec66f30147bafd455e7ad1
srihitha <srihithatsr@gmail.com>
2022-06-23 15:43:37 +0530

    Initial commit

---
+++ app/src/main/java/com/example/composenoteapp/ui/theme/Color.kt
@@ -0,0 +1,24 @@
+package com.example.composenoteapp.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple200 = Color(0xFFBB86FC)
+val Purple500 = Color(0xFF6200EE)
+val Purple700 = Color(0xFF3700B3)
+val Teal200 = Color(0xFF03DAC5)
+
+val DarkGray=Color(0xFF202020)
+val LightBlue=Color(0xFFD7E8DE)
+
+val RedOrange=Color(0xffffab91)
+val RedPink=Color(0xfff48fb1)
+val BabyBlue=Color(0xff81deea)
+val Violet=Color(0xffe7ed9b)
+val LightGreean=Color(0xffe7ed9b)
+
+
+val Primary = Color(0xFF0C9598)
+val lightColor = Color(0xFFF5FAFF)
+val darkColor = Color(0xFF001833)
+
+val errorColor = Color(0xFFCF6678)
---
+++ app/src/main/java/com/example/composenoteapp/ui/theme/Theme.kt
@@ -0,0 +1,49 @@
+package com.example.composenoteapp.ui.theme
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+
+private val DarkColorPalette = darkColors(
+        primary = Color.White,
+        background= DarkGray,
+        onBackground=Color.White,
+        surface= LightBlue,
+        onSurface= DarkGray
+       /* primaryVariant = Purple700,
+        secondary = Teal200*/
+)
+
+private val LightColorPalette = lightColors(
+        primary = Purple500,
+        primaryVariant = Purple700,
+        secondary = Teal200
+
+        /* Other default colors to override
+    background = Color.White,
+    surface = Color.White,
+    onPrimary = Color.White,
+    onSecondary = Color.Black,
+    onBackground = Color.Black,
+    onSurface = Color.Black,
+    */
+)
+
+@Composable
+fun ComposeNoteAppTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
+    val colors = if (darkTheme) {
+        DarkColorPalette
+    } else {
+        LightColorPalette
+    }
+
+    MaterialTheme(
+            colors = DarkColorPalette,
+            typography = Typography,
+            shapes = Shapes,
+            content = content
+    )
+}
