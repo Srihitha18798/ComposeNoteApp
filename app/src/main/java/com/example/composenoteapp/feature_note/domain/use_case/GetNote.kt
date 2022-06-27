1dc9b69c696b16af670abee89385d0ad689b7942
srihitha <srihithatsr@gmail.com>
2022-06-27 15:53:22 +0530

    Updated Code

---
+++ app/src/main/java/com/example/composenoteapp/feature_note/domain/use_case/GetNote.kt
@@ -0,0 +1,13 @@
+package com.example.composenoteapp.feature_note.domain.use_case
+
+import com.example.composenoteapp.feature_note.domain.model.Note
+import com.example.composenoteapp.feature_note.domain.repository.NoteRepository
+
+class GetNote(
+    private val repository: NoteRepository
+) {
+    suspend operator fun invoke(id:Int): Note?{
+
+        return repository.getNoteNyId(id)
+    }
+}
--- app/src/main/java/com/example/composenoteapp/feature_note/domain/util/NoteOrder.kt
+++ app/src/main/java/com/example/composenoteapp/feature_note/domain/util/NoteOrder.kt
@@ -5,4 +5,12 @@ sealed class NoteOrder(val orderType: OrderType){
     class Date(orderType: OrderType):NoteOrder(orderType)
     class Color(orderType: OrderType):NoteOrder(orderType)
 
+    fun copy(orderType: OrderType):NoteOrder{
+        return when(this){
+            is Title->Title(orderType)
+            is Date->Date(orderType)
+            is Color->Color(orderType)
+        }
+    }
+
 }
